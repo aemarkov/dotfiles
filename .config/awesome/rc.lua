d8e5fa3b8ac84f1218407d68a275931e1f270315
Markov Alex <markovalex95@gmail.com>
2020-08-22 17:24:50 +0300

    Use xcwd to open terminal in the current directory
    Useful to open one more terminal emulator in the same working directory
    as currently open terminal emulator

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -331,11 +331,9 @@ globalkeys = gears.table.join(
     -- Launch programs
      awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
               {description = "show main menu", group = "awesome"}),
-    awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
+    awful.key({ modkey,           }, "Return", function () awful.spawn.with_shell(terminal .. " --working-directory=$(xcwd)") end,
               {description = "open a terminal", group = "launcher"}),
-    awful.key({ modkey,           }, "z", function () awful.spawn(terminal) end,
-              {description = "open a terminal", group = "launcher"}),
-    awful.key({ modkey, "Control" }, "r", awesome.restart,
+        awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
