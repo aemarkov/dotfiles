5a02cd401c2fa59c16218d86692907e63fc160bb
Markov Alex <markovalex95@gmail.com>
2020-11-04 17:35:06 +0300

    Automatically open active window, even if it is on the different screen
    Useful to switch to another window/browser tab with Rofi

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -644,6 +644,20 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 
+-- Focus urgent tags automatically
+tag.connect_signal("property::urgent", function(t)
+                       awful.screen.focus(t.screen)
+                       if not(t.selected) then
+                           t:view_only()
+                       end
+end)
+
+-- Focus urgent clients automatically
+client.connect_signal("property::urgent", function(c)
+                          c.minimized = false
+                          c:jump_to()
+end)
+
 -- Disable borders on lone windows
 -- Handle border sizes of clients.
 for s = 1, screen.count() do screen[s]:connect_signal("arrange", function ()
